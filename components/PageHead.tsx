1d96f09f955d9e789978e70b7c82edcd6f8ddf3b
unknown <mihaigabrieldavid@outlook.com>
2022-01-29 19:48:38 +0200

    add viewport and favicon

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -4,6 +4,7 @@ const PageHead = () => (
   <Head>
     <title>Mihai-Gabriel David</title>
     <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </Head>
 );
 
